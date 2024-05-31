7d629682db46dd1e0079726aad75d426a1930991
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-31 14:36:30 +0700

    cek antrian penuh/kosong

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -17,4 +17,15 @@ public:
 		cin >> num;
 		cout << endl;
 
+		// Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+		}
+	}
 };
